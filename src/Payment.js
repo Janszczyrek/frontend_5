a5ab3e8c66ef65e6c8c7693df3fd2e16a5921460
Janszczyrek <janwalus@gmail.com>
2025-05-05 23:02:49 +0200

    fix code smell

--- src/Payment.js
+++ src/Payment.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from "axios";
 import { CartContext } from './Cart';
 
